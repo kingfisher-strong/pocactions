7e9e63fa97f10b9a6fc8b24b145eeda7d47b7d86
snk0202-p <snk0202@gmail.com>
2019-10-03 10:24:26 +0530

    test comm

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end('Hello Node!\n');
+  res.end('Hello github actions\n');
 });
 
 server.listen(port, () => {
